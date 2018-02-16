0352e37cceae184285ebcf6e5b44102da477f6bc
Daniel Shugarts <daniel.shugarts@gmail.com>
2018-02-16 13:55:06 -0600

    1-8 complete

--- joins-solution.sql
+++ joins-solution.sql
@@ -21,3 +21,23 @@ FROM warehouse_product
 JOIN products ON products.id=warehouse_product.product_id
 JOIN warehouse ON warehouse.id=warehouse_product.warehouse_id
 WHERE products.description='diet pepsi';
+
++-- 5.Get the number of orders for each customer. NOTE: It is OK if those without 
++-- orders are not included in results.
++SELECT customers.*, COUNT(orders.id) FROM customers
++JOIN addresses ON customers.id = addresses.customer_id
++JOIN orders ON orders.address_id = addresses.id
++GROUP BY customers.id;
++
++-- 6.How many customers do we have?
++SELECT COUNT(id)
++FROM customers;
++
++--7.How many products do we carry?
++SELECT COUNT(id)
++FROM products;
++
++--8.What is the total available on-hand quantity of diet pepsi?
++SELECT SUM(on_hand)
++FROM warehouse_product
++WHERE product_id=6;
